4059fb8e93f22ac8c04d8bd86dca69994da4d323
Philipp Bobek <philipp.bobek@mailbox.org>
2021-01-31 10:24:52 +0100

    Update Spring Boot to 2.4.2

--- src/main/java/de/bobek/spring/liquibase/customer/CustomerController.java
+++ src/main/java/de/bobek/spring/liquibase/customer/CustomerController.java
@@ -1,10 +1,13 @@
 package de.bobek.spring.liquibase.customer;
 
+import java.util.List;
+
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
@@ -12,9 +15,11 @@ import static java.util.stream.Collectors.toList;
 @RequiredArgsConstructor
 public class CustomerController {
 
+    @NonNull
     private final CustomerService customerService;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public List<String> getCustomers() {
         return customerService.getCustomers().stream()
                 .map(Customer::getName)
--- src/main/java/de/bobek/spring/liquibase/customer/CustomerService.java
+++ src/main/java/de/bobek/spring/liquibase/customer/CustomerService.java
@@ -1,17 +1,24 @@
 package de.bobek.spring.liquibase.customer;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.transaction.annotation.Propagation.MANDATORY;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class CustomerService {
 
+    @NonNull
     private final CustomerRepository customerRepository;
 
     @PostConstruct
@@ -20,6 +27,7 @@ public class CustomerService {
         log.info("Customer table contains {} entries", count);
     }
 
+    @Transactional(propagation = MANDATORY, readOnly = true)
     public List<Customer> getCustomers() {
         return customerRepository.findAll();
     }
